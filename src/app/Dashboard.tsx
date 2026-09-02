9c94d06504dfa026438bd64b740c956f84a017d9
Justine Ondap <j.ondap.149153.tc@umindanao.edu.ph>
2026-09-03 06:11:23 +0800

    Remove unused layout and index files; add Dashboard component and its styles

---
+++ src/app/Dashboard.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Dashboard() {
+  return (
+    <SafeAreaView>
+      <View>
+
+        <View>
+          <Text>Smart Home</Text>
+        </View>
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+
